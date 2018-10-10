92db5cb1b82d52cb690658e1f3015137ced269e8
RobertoR <soycplr@gmail.com>
2018-10-10 18:10:54 -0500

    postulacion

--- db.sql
+++ db.sql
@@ -18,6 +18,12 @@ CREATE TABLE proyecto (
 	PRIMARY KEY (id_proyecto)
 );
 
+CREATE TABLE pregunta (
+	id_pregunta int NOT NULL , 
+	pregunta text NOT NULL ,
+	PRIMARY KEY (id_pregunta)
+);
+
 
 CREATE TABLE equipo (
 	id_proyecto BIGINT NOT NULL ,
@@ -35,7 +41,55 @@ CREATE TABLE representante (
 );
 
 
+CREATE TABLE respuesta_proyecto (
+	id_proyecto BIGINT NOT NULL, 
+	id_pregunta int NOT NULL ,
+	respuesta text NOT NULL,
+	FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto),
+	FOREIGN KEY (id_pregunta) REFERENCES pregunta(id_pregunta)
+);
+
+INSERT INTO pregunta VALUES
+	(1, '¿Qué producto o servicio produces?'),
+	(2, '¿Por qué́ consideras que la solución que propones es innovadora o cuenta con un valor agregado?'),
+	(3, '¿Qué problemática resuelves con tu producto o servicio?'),
+	(4, '¿Quiénes, cuantos y en donde se beneficiarán con tú propuesta?'),
+	(5, '¿Te consideras un emprendedor? ¿Por qué?'),
+	(6, '¿Qué mensaje le darías al cliente para promover el servicio que propones?'),
+	(7, 'Te invitamos a que presentes tu proyecto y tu equipo en un video de no más de 2 minutos (puedes subirlo a cualquier plataforma de videos como youtube y poner el link en el espacio de texto, por ejemplo: https://www.youtube.com/watch?v=klVWGHtRTuE )');
+
+
+
+
+
+
+/* TESTING PURPOSES
+
+	INSERT INTO respuesta_proyecto VALUES
+	(15, 1, 'test1'),
+	(15, 3, 'test3');
+
+60000 * 30
+
+ HMiejHqFN1uIIzxon4tcjNñ == $2b$08$b.fSCZ97tDvZbPZa5C3b6OfSLT4xjZrixAliS6.5ikTvB7GKI39om
+ 
+ HMiejHqFN1uIIzxon4tcjNñ
+ $2b$08$XKTqzsDXMR.AcTjoy/3Rsu5Ru63mWGCGv.9PtJjztfhCbeJYMc.n6
+
+select * from alumnos;
+select * from proyecto;
+select * from pregunta;
+select * from equipo;
+select * from representante;
+select * from respuesta_proyecto;
+
+
 delete from equipo;
 delete from representante;
 delete from alumnos;
+delete from pregunta;
 delete from proyecto;
+delete from respuesta_proyecto;
+*/
+
+
--- improde/app.js
+++ improde/app.js
@@ -6,13 +6,15 @@ const mysql = require('mysql');
 const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const port = 3003;
-const session = require('express-session');
+const session = require('express-session'); 
+const bcrypt = require('bcrypt');
+const saltRounds = 8;
 
 let con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: 'localhost',
+    user: 'root',
     port: '3306',
-    password: "car1118",
+    password: 'car1118',
     database: 'improde'
 });
 con.connect(err => {
@@ -24,43 +26,74 @@ con.connect(err => {
 app.use(session({
     secret: 'asdfghjkl',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
 }));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localStrategy(
-    function(username, password, done) {
+    function (username, password, done) {
         con.query('SELECT id_proyecto, contrasena FROM proyecto WHERE correo = ?', [username], (err, results) => {
             if (err) done(err);
-            if(results.length> 0 && results[0].contrasena===password){
-                return done(null, {id_proyecto: results[0].id_proyecto});
-            } 
-            return done(null,false);
-        });
+            if(!results[0])  return done(null, false);
 
+            bcrypt.compare(password, results[0].contrasena, function(err, res) {
+                if (err) done(err);
+                if(res) return done(null, { id_proyecto: results[0].id_proyecto });
+                return done(null, false);
+            });
+        });
     }
 ));
-
-
+            
 
 app.get('/registro', (req, res) => res.render('Registro.ejs'));
-app.get('/', authenticationMiddleware(),(req, res) =>  res.render('Postulacion.ejs'));
-app.get('/login', authenticationMiddleware(),(req, res) =>  res.redirect('/'));
-app.get('/misdatos', authenticationMiddleware(),(req, res) =>  res.render('DatosPersonales.ejs'));
+app.get('/login', (req, res) => res.render('Login.ejs'));
 app.get('/recursos', authenticationMiddleware(), (req, res) => res.render('Recursos.ejs'));
-app.post('/agregarProyecto', (req, res) => {
+app.get('/misdatos', authenticationMiddleware(), (req, res) => res.render('DatosPersonales.ejs'));
+
+app.get('/', authenticationMiddleware(), (req, res) => {
+    let idProyecto = req.session.passport.user.id_proyecto;
+    let sqlRespuesta = 'SELECT * FROM respuesta_proyecto WHERE id_proyecto = ? ORDER BY id_pregunta';
+    con.query(sqlRespuesta, [idProyecto], (err, result) => {
+        if (err){
+            console.log(err);
+            return;
+        } 
+        res.render('Postulacion.ejs', { 
+            respuesta1: result[0].respuesta,
+            respuesta2: result[1].respuesta,
+            respuesta3: result[2].respuesta,
+            respuesta4: result[3].respuesta,
+            respuesta5: result[4].respuesta,
+            respuesta6: result[5].respuesta,
+            respuesta7: result[6].respuesta});
+    });
+});
+
+
+app.post('/registro', (req, res) => {
     let sqlProyecto = 'INSERT INTO proyecto (nombre_proyecto, correo, contrasena) VALUES ?';
     let sqlAlumnos = 'INSERT INTO alumnos (matricula, nombre, carrera, cuatrimestre) VALUES ?';
     let sqlRepresentante = 'INSERT INTO representante (id_proyecto, matricula) VALUES ?';
     let sqlEquipo = 'INSERT INTO equipo (id_proyecto, matricula) VALUES ?';
+    let sqlRespuesta = 'INSERT INTO respuesta_proyecto (id_proyecto, id_pregunta, respuesta) VALUES ?';
 
-    let proyecto = [[req.body.nombreProyecto, req.body.email, req.body.password]];
-    con.query(sqlProyecto, [proyecto], (err, result) => {
-        if (err) console.log(err);
-        queryAll(result.insertId);
-    });
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+            let password = hash;
+            if(err){
+                password = req.body.password
+            }
+            let proyecto = [[req.body.nombreProyecto, req.body.email, password]];
+            con.query(sqlProyecto, [proyecto], (err, result) => {
+                if (err){
+                    console.log(err);
+                    return;
+                } 
+                queryAll(result.insertId);
+            });
+      });
 
     function queryAll(lastInsertID) {
         let alumnos = [
@@ -71,12 +104,18 @@ app.post('/agregarProyecto', (req, res) => {
             [req.body.matricula5, req.body.nombre5, req.body.licenciatura5, req.body.cuatrimestre5]
         ];
         con.query(sqlAlumnos, [alumnos], (err, result) => {
-            if (err) console.log(err);
+            if (err){
+                console.log(err);
+                return;
+            } 
         });
 
         let representante = [[lastInsertID, req.body.matricula1]];
         con.query(sqlRepresentante, [representante], (err, result) => {
-            if (err) console.log(err);
+            if (err){
+                console.log(err);
+                return;
+            } 
         });
 
         let equipo = [
@@ -87,14 +126,55 @@ app.post('/agregarProyecto', (req, res) => {
             [lastInsertID, req.body.matricula5]
         ];
         con.query(sqlEquipo, [equipo], (err, result) => {
+            if (err){
+                console.log(err);
+                return;
+            } 
+        });
+
+        let respuesta = [
+            [lastInsertID, 1, ''],
+            [lastInsertID, 2, ''],
+            [lastInsertID, 3, ''],
+            [lastInsertID, 4, ''],
+            [lastInsertID, 5, ''],
+            [lastInsertID, 6, ''],
+            [lastInsertID, 7, '']
+        ];
+        con.query(sqlRespuesta, [respuesta], (err, result) => {
+            if (err){
+                console.log(err);
+                return;
+            } 
+        });
+    }
+    res.redirect('/login');
+});
+
+
+app.post('/postulacion', authenticationMiddleware(), (req, res) => {
+    let idProyecto = req.session.passport.user.id_proyecto;
+    let arrayRespuestas = [
+        req.body.respuesta1,
+        req.body.respuesta2,
+        req.body.respuesta3,
+        req.body.respuesta4,
+        req.body.respuesta5,
+        req.body.respuesta6,
+        req.body.respuesta7,
+    ];
+    for(let i=0;i<arrayRespuestas.length;i++){
+        con.query('UPDATE respuesta_proyecto SET respuesta = ? WHERE id_pregunta = ? AND id_proyecto = ?', 
+        [arrayRespuestas[i],i+1,idProyecto], (err, results) => {
             if (err) console.log(err);
         });
     }
     res.redirect('/');
 });
 
-app.post('/', passport.authenticate('local',
-    {
+
+app.post('/login', passport.authenticate('local',
+{
         successRedirect: '/',
         failureRedirect: '/login'
 }));
@@ -108,9 +188,12 @@ passport.deserializeUser(function (id, done) {
 });
 function authenticationMiddleware() {
     return (req, res, next) => {
-        console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
-        if (req.isAuthenticated()) return next();
-        res.render('Login.ejs');
+        if (req.isAuthenticated()){
+ //           console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+            req.session.cookie.maxAge = 60000 * 120;
+            return next();
+        } 
+        res.redirect('/login');
     }
 }
 
